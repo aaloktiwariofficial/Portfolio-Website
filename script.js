369814ce295f62d1d04857eab3d85384c8b7dc35
aaloktiwariofficial <tiwari.aalok24@gmail.com>
2022-08-14 17:29:06 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector('.close').style.display='none';
+document.querySelector('.menu').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector('.ham').style.display='inline';
+        document.querySelector('.close').style.display='none';
+    }
+    else{
+        document.querySelector('.ham').style.display='none';
+        setTimeout(()=>{
+        document.querySelector('.close').style.display='inline';
+        }, 200);
+    }
+}); 
